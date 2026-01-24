b452be6c463deab962e068c53f31f1355cba89ec
hrvxf <hsajame3@gmail.com>
2026-01-24 16:02:33 +0000

    updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       </head>
       <body className="min-h-screen bg-lightbg text-ink flex flex-col">
         <Header />
-        <main className="flex-1">{children}</main>
+        <main className="flex-1 pt-6 md:pt-8">{children}</main>
         <Footer />
       </body>
     </html>
