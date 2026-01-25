bf41f01f9a6ec12762292d6dbef41b83ad36ebfa
hrvxf <hsajame3@gmail.com>
2026-01-25 16:02:59 +0000

    update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className="bg-lightbg">
       <head>
         <meta
           name="viewport"
