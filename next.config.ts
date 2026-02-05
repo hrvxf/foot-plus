213d1500a7505f77f95a4d5a82449c31bd1f1f10
hrvxf <hsajame3@gmail.com>
2026-02-05 10:56:49 +0000

    updated google map api

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  env: {
-    NEXT_PUBLIC_GOOGLE_MAPS_API_KEY:
-      process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
