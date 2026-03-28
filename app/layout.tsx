9b95e27a51349635eec8b6d2ef5945416676c87c
hrvxf <166922826+hrvxf@users.noreply.github.com>
2026-03-28 18:05:53 +0000

    Add Google Analytics gtag to global layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -134,6 +134,18 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       </head>
 
       <body className="min-h-screen bg-lightbg text-ink flex flex-col">
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-7X749Z34XF"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-7X749Z34XF');
+          `}
+        </Script>
         <Script id="meta-pixel" strategy="afterInteractive">
           {`
             !function(f,b,e,v,n,t,s)
