2db761ce8d23644eacbc66a8e859b0e43e63b74b
hrvxf <hsajame3@gmail.com>
2026-03-28 19:21:35 +0000

    Add all remaining local changes

---
+++ foot-plus-website/.next/dev/static/chunks/app_layout_tsx_1cf6b850._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/app_globals_71f961d1.css",
+  "static/chunks/app_components_7bcfe803._.js",
+  "static/chunks/node_modules_e22101b3._.js"
+],
+    source: "dynamic"
+});
